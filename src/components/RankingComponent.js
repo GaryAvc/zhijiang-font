57d470a1f7ec3791aefb1e7d360beb78bce0a71f
Gary Xu <xjs1997524@gmail.com>
2020-08-12 16:50:51 +0800

    <make Ranking>[RaningComponent] 1. finish modify ranking component

--- src/components/RankingComponent.js
+++ src/components/RankingComponent.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Table } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function RenderDish({ record }) {
+function RenderRank({ rank }) {
 	// Outputs the date and time in Mon dd, YYYY, H:MM:SS AM/PM format
 	return (
 		<tr>
-			<th scope="row">{record.user}</th>
-			<td>{record.rank}</td>
-			<td>{record.score}</td>
-			<td>{record.totalScore}</td>
+			<th scope="row">{rank.user}</th>
+			<td>{rank.rank}</td>
+			<td>{rank.totalScore}</td>
+			<td>{rank.case1Score}</td>
+			<td>{rank.case2Score}</td>
+			<td>{rank.case3Score}</td>
 		</tr>
 	);
 }
 
 const Ranking = (props) => {
-	const singleRecord = props.ranks.ranks.map((record) => {
-		return <RenderDish record={record} />;
+	const singleRank = props.ranks.ranks.map((rank) => {
+		return <RenderRank rank={rank} />;
 	});
 
 	return (
@@ -36,12 +38,14 @@ const Ranking = (props) => {
 			<div className="row">
 				<Table>
 					<thead>
-						<th>#</th>
-						<th>名称</th>
-						<th>得分</th>
-						<th>时间</th>
+						<th>用户</th>
+						<th>排名</th>
+						<th>总得分</th>
+						<th>题1得分</th>
+						<th>题2得分</th>
+						<th>题3得分</th>
 					</thead>
-					<tbody>{singleRecord}</tbody>
+					<tbody>{singleRank}</tbody>
 				</Table>
 			</div>
 		</div>
