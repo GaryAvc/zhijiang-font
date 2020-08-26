503a34262b78fccc4abf7505c41eddf585eb45ab
Gary Xu <xjs1997524@gmail.com>
2020-08-26 17:31:10 +0800

    < SUCCESS - finish ranking part>[ RankingComponent ] 1. well all finished at this stage 2. :) well done 3. have a nice day

--- src/components/RankingComponent.js
+++ src/components/RankingComponent.js
@@ -3,17 +3,30 @@ import { Breadcrumb, BreadcrumbItem, Table } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 // todo - Start to modify the code to updated to ip server
-
+//[{"caseScore":[{"caseId":"milestone2_demo/AITownReconstructed_V0103_200518/apollo_add_basic/case.json","caseScore":100.0}],
+// "rank":1,"totalScore":100.0,"username":"test"}]
 function RenderRank({ rank }) {
-	// Outputs the date and time in Mon dd, YYYY, H:MM:SS AM/PM format
+	const singleCase = rank.caseScore.map((oneCaseScore) => {
+		return <QuestionDetail oneCaseScore={oneCaseScore} />;
+	});
+
 	return (
 		<tr>
-			<th scope="row">{rank.user}</th>
+			<th scope="row">{rank.username}</th>
 			<td>{rank.rank}</td>
 			<td>{rank.totalScore}</td>
-			<td>{rank.case1Score}</td>
-			<td>{rank.case2Score}</td>
-			<td>{rank.case3Score}</td>
+			{singleCase}
+		</tr>
+	);
+}
+
+function QuestionDetail({ oneCaseScore }) {
+	return (
+		<tr>
+			<td>
+				{oneCaseScore.caseId} <br />
+				得分: {oneCaseScore.caseScore}
+			</td>
 		</tr>
 	);
 }
@@ -43,9 +56,12 @@ const Ranking = (props) => {
 						<th>用户</th>
 						<th>排名</th>
 						<th>总得分</th>
-						<th>题1得分</th>
-						<th>题2得分</th>
-						<th>题3得分</th>
+						<th>题1</th>
+						<th>题2</th>
+						<th>题3</th>
+						<th>题4</th>
+						<th>题5</th>
+						<th>题6</th>
 					</thead>
 					<tbody>{singleRank}</tbody>
 				</Table>
